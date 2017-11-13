186078950cbbc5bde3c8b9198e944b30fb2c66fb
linyuetian <linyuetian@gd.chinamobile.com>
2017-11-13 20:12:01 +0800

    part 6-7

--- src/main/java/com/imooc/service/impl/OrderServiceImpl.java
+++ src/main/java/com/imooc/service/impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.imooc.service.impl;
 
+import com.imooc.converter.OrderMaster2OrderDTOConverter;
 import com.imooc.dataobject.OrderDetail;
 import com.imooc.dataobject.OrderMaster;
 import com.imooc.dataobject.ProductInfo;
@@ -17,8 +18,10 @@ import com.imooc.utils.KeyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
@@ -79,12 +82,30 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public OrderDTO findOne(String orderId) {
-        return null;
+        OrderMaster orderMaster = orderMasterReposity.findOne(orderId);
+        if(orderMaster == null){
+            throw new SellException(ResultEnum.ORDER_NOT_EXIST);
+        }
+
+        List<OrderDetail> orderDetailList = orderDetailReposity.findByOrderId(orderId);
+        if(CollectionUtils.isEmpty(orderDetailList)){
+            throw new SellException(ResultEnum.ORDERDETAIL_NOT_EXIST);
+        }
+
+        OrderDTO orderDTO = new OrderDTO();
+        BeanUtils.copyProperties(orderMaster,orderDTO);
+        orderDTO.setOrderDetailList(orderDetailList);
+        return orderDTO;
     }
 
     @Override
     public Page<OrderDTO> findList(String buyerId, Pageable pageable) {
-        return null;
+        Page<OrderMaster> orderMasterPage = orderMasterReposity.findByBuyerId(buyerId,pageable);
+
+        List<OrderDTO> orderDTOList = OrderMaster2OrderDTOConverter.convert(orderMasterPage.getContent());
+
+        Page<OrderDTO> orderDTOPage = new PageImpl<OrderDTO>(orderDTOList,pageable,orderMasterPage.getTotalElements());
+        return orderDTOPage;
     }
 
     @Override
--- src/test/java/com/imooc/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/imooc/service/impl/OrderServiceImplTest.java
@@ -2,12 +2,15 @@ package com.imooc.service.impl;
 
 import com.imooc.dataobject.OrderDetail;
 import com.imooc.dto.OrderDTO;
+import com.imooc.respository.OrderMasterReposity;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -22,6 +25,8 @@ public class OrderServiceImplTest {
 
     private final String BUYERID = "110";
 
+    private final String ORDERID= "1510573178016198674";
+
     @Test
     public void create() throws Exception {
         OrderDTO orderDTO = new OrderDTO();
@@ -33,21 +38,16 @@ public class OrderServiceImplTest {
         //cart
         List<OrderDetail> orderDetailList = new ArrayList<>();
         OrderDetail o1 =  new OrderDetail();
-        o1.setProductId("123458");
+        o1.setProductId("123456");
         o1.setProductQuantity(1);
         orderDetailList.add(o1);
 
-
-        OrderDetail o2 = new OrderDetail();
-        o2.setProductId("123457");
-        o2.setProductQuantity(2);
-        orderDetailList.add(o2);
 
         orderDTO.setOrderDetailList(orderDetailList);
 
         OrderDTO result =  orderService.create(orderDTO);
 
-        log.info("［创建订单］result={}",result);
+        log.info("[创建订单]result={}",result);
 
         Assert.assertNotNull(result);
 
@@ -56,10 +56,17 @@ public class OrderServiceImplTest {
 
     @Test
     public void findOne() throws Exception {
+        OrderDTO result = orderService.findOne(ORDERID);
+        log.info("[查询单个订单]result={}",result);
+        Assert.assertEquals(ORDERID,result.getOrderId());
     }
 
     @Test
     public void findList() throws Exception {
+        PageRequest request = new PageRequest(0,2);
+        Page<OrderDTO> orderDTOPage = orderService.findList(BUYERID,request);
+
+        Assert.assertNotEquals(0,orderDTOPage.getTotalElements());
     }
 
     @Test
